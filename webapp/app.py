2ba85108cdf7fd38173066c3fb4d2c6b964ac69a
immortalcodes <21112002mj@gmail.com>
2025-02-24 16:00:22 +0530

    logging errors

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 from flask import render_template, request
 from urllib.parse import parse_qs, urlencode
@@ -20,12 +21,12 @@ session = requests.Session()
 
 @app.errorhandler(Exception)
 def render_error_page(error):
+    app.logger.error(f"Error occurred: {error}", exc_info=os.environ.get("DISPLAY_FULL_TRACEBACK").lower() == "true")
     error_code = getattr(error, "code", 500)
     error_message = getattr(error, "description", "Something went wrong!")
     return render_template(
         "error.html", error_code=int(error_code), error_message=error_message
     )
-
 
 blog_views = BlogViews(
     api=BlogAPI(
